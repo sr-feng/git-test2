9d3ef7cfdb6c5cfc98100e3bd559b0202bdcc294
sr-feng <73383085+sr-feng@users.noreply.github.com>
2022-07-14 21:03:07 +0800

    pull commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("master master");
         System.out.println("hot-fix hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
