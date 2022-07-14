494340af5fcded0d71fa608311885ad71efde6a5
笨笨同学 <heimaTest@itcast.cn>
2022-07-14 20:58:04 +0800

    push commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("hot-fix test");
         System.out.println("master master");
         System.out.println("hot-fix hot-fix");
+        System.out.println("push test");
     }
 }
