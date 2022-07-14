88aa8ea9cbcc35a7fb83112bc4d539373c51acae
笨笨同学 <heimaTest@itcast.cn>
2022-07-14 10:46:49 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git---hot-fix");
+        System.out.println("hot-fix test");
     }
 }
